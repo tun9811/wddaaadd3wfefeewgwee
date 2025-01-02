827cb527737b8e777f53dcdeca457b7d5383f07a
x3sharkx2z <bevz91741@gmail.com>
2025-01-02 18:58:24 +0700

    Update AutoFarmtest.lua

--- AutoFarmtest.lua
+++ AutoFarmtest.lua
@@ -89,7 +89,7 @@ function CheckQuest()
             CFrameMon = CFrame.new(1134, 197, -1543) 
         elseif MyLevel == 120 or MyLevel <= 149 or SelectMonter ==  "Chief Petty Officer" then
             Mon = "Chief Petty Officer"
-            NameQuest = "MarineQuest"
+            NameQuest = "MarineQuest2"
             LevelQuest = 1
             NameMon = "Chief Petty Officer"
             CFrameQuest = CFrame.new(-5039.58643, 27.3500385, 4324.68018, -0.422094226, -0, -0.906552136, 0, 1, -0, 0.906552136, 0, -0.422094226)
@@ -100,7 +100,7 @@ function CheckQuest()
             LevelQuest = 1
             NameMon = "Sky Bandit"
             CFrameQuest = CFrame.new(-4839.53027, 716.368591, -2619.44165, 0.139098823, 0, 0.990278423, 0, 1, 0, -0.990278423, 0, 0.139098823)
-            CFrameMon = CFrame.new(-4961, 506, -2979) 
+            CFrameMon = CFrame.new(-4950, 349, -2887) 
         elseif MyLevel == 175 or MyLevel <= 189 or SelectMonter ==  "Dark Master" then
             Mon = "Dark Master"
             NameQuest = "SkyQuest"
@@ -2747,7 +2747,6 @@ spawn(function()
                 if game:GetService("Workspace").Map.Jungle.Final.Part.Transparency == 0 then
                     if game:GetService("Workspace").Map.Jungle.QuestPlates.Door.Transparency == 0 then
                         if (CFrame.new(-1612.55884, 36.9774132, 148.719543, 0.37091279, 3.0717151e-09, -0.928667724, 3.97099491e-08, 1, 1.91679348e-08, 0.928667724, -4.39869794e-08, 0.37091279).Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 100 then
-                            ATween(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame)
                             wait(1)
                             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map.Jungle.QuestPlates.Plate1.Button.CFrame
                             wait(1)
@@ -2760,15 +2759,15 @@ spawn(function()
                             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map.Jungle.QuestPlates.Plate5.Button.CFrame
                             wait(1)
                         else
-                            ATween(CFrame.new(-1612.55884, 36.9774132, 148.719543, 0.37091279, 3.0717151e-09, -0.928667724, 3.97099491e-08, 1, 1.91679348e-08, 0.928667724, -4.39869794e-08, 0.37091279))
+                            TP1(CFrame.new(-1612.55884, 36.9774132, 148.719543, 0.37091279, 3.0717151e-09, -0.928667724, 3.97099491e-08, 1, 1.91679348e-08, 0.928667724, -4.39869794e-08, 0.37091279))
                         end
                     else
                         if game:GetService("Workspace").Map.Desert.Burn.Part.Transparency == 0 then
                             if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Torch") or game.Players.LocalPlayer.Character:FindFirstChild("Torch") then
                                 EquipWeapon("Torch")
-                                ATween(CFrame.new(1114.61475, 5.04679728, 4350.22803, -0.648466587, -1.28799094e-09, 0.761243105, -5.70652914e-10, 1, 1.20584542e-09, -0.761243105, 3.47544882e-10, -0.648466587))
+                                TP1(CFrame.new(1114.61475, 5.04679728, 4350.22803, -0.648466587, -1.28799094e-09, 0.761243105, -5.70652914e-10, 1, 1.20584542e-09, -0.761243105, 3.47544882e-10, -0.648466587))
                               else
-                              ATween(CFrame.new(-1610.00757, 11.5049858, 164.001587, 0.984807551, -0.167722285, -0.0449818149, 0.17364943, 0.951244235, 0.254912198, 3.42372805e-05, -0.258850515, 0.965917408))
+                              TP1(CFrame.new(-1610.00757, 11.5049858, 164.001587, 0.984807551, -0.167722285, -0.0449818149, 0.17364943, 0.951244235, 0.254912198, 3.42372805e-05, -0.258850515, 0.965917408))
                             end
                         else
                             if game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ProQuestProgress","SickMan") ~= 0 then
@@ -2784,10 +2783,10 @@ spawn(function()
                                     game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ProQuestProgress","RichSon")
                                 elseif game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("ProQuestProgress","RichSon") == 0 then
                                 if game:GetService("Workspace").Enemies:FindFirstChild("Mob Leader") or game:GetService("ReplicatedStorage"):FindFirstChild("Mob Leader") then
-                                    ATween(CFrame.new(-2874.97021484375, 20.01312828063965, 5415.25244140625))---ควย
+                                    TP1(CFrame.new(-2874.97021484375, 20.01312828063965, 5415.25244140625))---ควย
                                         for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
                                             if v.Name == "Mob Leader" then
-                                               if game:GetService("Workspace").Enemies:FindFirstChild("Mob Leader [Lv. 120] [Boss]") then
+                                               if game:GetService("Workspace").Enemies:FindFirstChild("Mob Leader") then
                                                 if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
                                                     repeat task.wait()
                                                     AutoHaki()
@@ -2795,7 +2794,7 @@ spawn(function()
                                                     v.HumanoidRootPart.CanCollide = false
                                                     v.Humanoid.WalkSpeed = 0
                                                     v.HumanoidRootPart.Size = Vector3.new(80,80,80)                             
-                                                    ATween(v.HumanoidRootPart.CFrame * Pos)
+                                                    TP1(v.HumanoidRootPart.CFrame * Pos)
                                                     game:GetService("VirtualUser"):CaptureController()
                                                     game:GetService("VirtualUser"):Button1Down(Vector2.new(1280,672))
                                                     sethiddenproperty(game:GetService("Players").LocalPlayer,"SimulationRadius",math.huge)
@@ -2803,7 +2802,7 @@ spawn(function()
                                                  end
                                             end
                                             if game:GetService("ReplicatedStorage"):FindFirstChild("Mob Leader") then
-                                                ATween(game:GetService("ReplicatedStorage"):FindFirstChild("Mob Leader").HumanoidRootPart.CFrame * Farm_Mode)
+                                                TP1(game:GetService("ReplicatedStorage"):FindFirstChild("Mob Leader").HumanoidRootPart.CFrame * Pos)
                                             end
                                         end
                                     end
@@ -2813,7 +2812,7 @@ spawn(function()
                                     wait(0.5)
                                     EquipWeapon("Relic")
                                     wait(0.5)
-                                    ATween(CFrame.new(-1404.91504, 29.9773273, 3.80598116, 0.876514494, 5.66906877e-09, 0.481375456, 2.53851997e-08, 1, -5.79995607e-08, -0.481375456, 6.30572643e-08, 0.876514494))
+                                    TP1(CFrame.new(-1404.91504, 29.9773273, 3.80598116, 0.876514494, 5.66906877e-09, 0.481375456, 2.53851997e-08, 1, -5.79995607e-08, -0.481375456, 6.30572643e-08, 0.876514494))
                                 end
                             end
                         end
@@ -2825,7 +2824,7 @@ spawn(function()
                                 if v.Name == "Saber Expert" then
                                     repeat task.wait()
                                         EquipWeapon(_G.SelectWeapon)
-                                        ATween(v.HumanoidRootPart.CFrame * Pos)
+                                        TP1(v.HumanoidRootPart.CFrame * Pos)
                                         v.HumanoidRootPart.Size = Vector3.new(60, 60, 60)
                                         v.HumanoidRootPart.Transparency = 1
                                         v.Humanoid.JumpPower = 0
